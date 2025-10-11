6610bc040edeeec73121858320c4b0da78304eaa
dongju.lim <dongju.lim@nts-corp.com>
2025-10-11 21:37:32 +0900

    Initial commit

---
+++ src/main/java/com/exxdx/exxdx_api/ExxdxApiApplication.java
@@ -0,0 +1,13 @@
+package com.exxdx.exxdx_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExxdxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExxdxApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exxdx/exxdx_api/ExxdxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exxdx.exxdx_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExxdxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
